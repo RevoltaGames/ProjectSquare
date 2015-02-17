32cb73fbd82fd146ae7127fc426eac89104595e8
César Antonio Enrique Ramirez <caenrique93@gmail.com>
2015-02-17 16:25:04 +0100

    project ready

---
+++ android/src/com/revoltagames/projectsquare/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.revoltagames.projectsquare.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.revoltagames.projectsquare.ProjectSquare;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProjectSquare(), config);
+	}
+}
