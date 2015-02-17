3bff337d641048e05b4f00b36904df5b65a41cc9
César Antonio Enrique Ramirez <caenrique93@gmail.com>
2015-02-17 17:17:48 +0100

    basic structure

---
+++ core/src/com/revoltagames/projectsquare/GameStates/Play.java
@@ -0,0 +1,7 @@
+package com.revoltagames.projectsquare.GameStates;
+
+/**
+ * Created by caenrique93 on 17/02/15.
+ */
+public class Play {
+}
---
+++ core/src/com/revoltagames/projectsquare/Managers/GameStateManager.java
@@ -0,0 +1,7 @@
+package com.revoltagames.projectsquare.Managers;
+
+/**
+ * Created by caenrique93 on 17/02/15.
+ */
+public class GameStateManager {
+}
