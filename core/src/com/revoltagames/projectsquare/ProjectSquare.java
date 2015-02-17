69464f6bb879c17865d79066eb06cc964652e895
Truji92 <alejandro.trujillo.7@gmail.com>
2015-02-17 17:51:23 +0100

    Codigo de arranque del juego

--- core/src/com/revoltagames/projectsquare/ProjectSquare.java
+++ core/src/com/revoltagames/projectsquare/ProjectSquare.java
@@ -3,20 +3,39 @@ package com.revoltagames.projectsquare;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.revoltagames.projectsquare.Managers.GameStateManager;
 
 public class ProjectSquare extends ApplicationAdapter {
 
+    private GameStateManager gameStateManager;
+
+    public static int WIDTH;
+    public static int HEIGTH;
+
+    private OrthographicCamera camera;
 	
 	@Override
 	public void create () {
-	}
+
+        WIDTH = Gdx.graphics.getWidth();
+        HEIGTH = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera(WIDTH, HEIGTH);
+        camera.translate(WIDTH/2, HEIGTH/2);
+        camera.update();
+
+        gameStateManager = new GameStateManager();
+    }
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1); //White
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        gameStateManager.update(Gdx.graphics.getDeltaTime());
+        gameStateManager.draw();
 	}
 }
