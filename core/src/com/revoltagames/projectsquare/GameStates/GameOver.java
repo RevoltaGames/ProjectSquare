43812c8001be092408505fe2a17b627f30331706
César Antonio Enrique Ramirez <caenrique93@gmail.com>
2015-02-18 22:33:50 +0100

    The GameOver class is working but not finished

--- core/src/com/revoltagames/projectsquare/GameStates/GameOver.java
+++ core/src/com/revoltagames/projectsquare/GameStates/GameOver.java
@@ -1,8 +1,12 @@
 package com.revoltagames.projectsquare.GameStates;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.revoltagames.projectsquare.Entities.CircularButton;
 import com.revoltagames.projectsquare.Entities.ImageCircularButton;
+import com.revoltagames.projectsquare.Managers.ColorManager;
 import com.revoltagames.projectsquare.Managers.GameStateManager;
+import com.revoltagames.projectsquare.ProjectSquare;
 
 /**
  * Created by caenrique93 on 17/02/15.
@@ -10,12 +14,29 @@ import com.revoltagames.projectsquare.Managers.GameStateManager;
 public class GameOver extends GameState {
 
     String gameOverText;
+    ShapeRenderer renderer;
+
+    /*
+    Estos botones son provisionales, cuanto esten las imagenes se usaran
+    los ImageCircularButton
+    */
+    CircularButton boton1;
+    CircularButton boton2;
+
     ImageCircularButton menuButton;
     ImageCircularButton retryButton;
 
     protected GameOver(GameStateManager gsm) {
         super(gsm);
-        //menuButton = new ImageCircularButton(ProjectSquare.WIDTH / 4, ProjectSquare.HEIGTH / 4, );
+        int unitStep = ProjectSquare.WIDTH/8;
+        boton1 = new CircularButton(unitStep*2,
+                ProjectSquare.HEIGTH/4,
+                unitStep,
+                ColorManager.LIGHT_BLUE);
+        boton2 = new CircularButton(unitStep*6,
+                ProjectSquare.HEIGTH/4,
+                unitStep,
+                ColorManager.LIGHT_GREEN);
     }
 
     @Override
@@ -30,13 +51,20 @@ public class GameOver extends GameState {
 
     @Override
     public void draw() {
-        // TODO
+        renderer.begin(ShapeRenderer.ShapeType.Filled);
+        boton1.draw(renderer);
+        boton2.draw(renderer);
+        renderer.end();
     }
 
     @Override
     public void handleInput() {
         if(Gdx.input.justTouched()) {
-            // TODO
+            if (boton1.touched(Gdx.input.getX(), Gdx.input.getY())) {
+                gsm.setState(new Play(this.gsm));
+            } else if (boton2.touched(Gdx.input.getX(), Gdx.input.getY())) {
+                gsm.setState(new Menu(this.gsm));
+            }
         }
     }
 
