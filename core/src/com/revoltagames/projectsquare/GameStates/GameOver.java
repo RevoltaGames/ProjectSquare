e4c3595184064c3dd4e50a565f837b4dcc3d6591
César Antonio Enrique Ramirez <caenrique93@gmail.com>
2015-02-18 17:03:42 +0100

    It's not usable

--- core/src/com/revoltagames/projectsquare/GameStates/GameOver.java
+++ core/src/com/revoltagames/projectsquare/GameStates/GameOver.java
@@ -1,5 +1,7 @@
 package com.revoltagames.projectsquare.GameStates;
 
+import com.badlogic.gdx.Gdx;
+import com.revoltagames.projectsquare.Entities.ImageCircularButton;
 import com.revoltagames.projectsquare.Managers.GameStateManager;
 
 /**
@@ -7,28 +9,35 @@ import com.revoltagames.projectsquare.Managers.GameStateManager;
  */
 public class GameOver extends GameState {
 
+    String gameOverText;
+    ImageCircularButton menuButton;
+    ImageCircularButton retryButton;
+
     protected GameOver(GameStateManager gsm) {
         super(gsm);
+        //menuButton = new ImageCircularButton(ProjectSquare.WIDTH / 4, ProjectSquare.HEIGTH / 4, );
     }
 
     @Override
     public void init() {
-
+        gameOverText = "jaja you loser!!";
     }
 
     @Override
     public void update(float dt) {
-
+        handleInput();
     }
 
     @Override
     public void draw() {
-
+        // TODO
     }
 
     @Override
     public void handleInput() {
-
+        if(Gdx.input.justTouched()) {
+            // TODO
+        }
     }
 
     @Override
---
+++ core/src/com/revoltagames/projectsquare/Managers/ResourceManager.java
@@ -0,0 +1,22 @@
+package com.revoltagames.projectsquare.Managers;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * Created by caenrique93 on 18/02/15.
+ */
+public class ResourceManager {
+
+    AssetManager assetManager;
+
+    public ResourceManager() {
+        this.assetManager = new AssetManager();
+    }
+
+    public Texture getTexture(String id) {
+        // TODO
+        return null;
+    }
+}
+
