af82a103039b15f340f40e3cf9fee47e9b2c698e
César Antonio Enrique Ramirez <caenrique93@gmail.com>
2015-04-02 13:25:49 +0200

    añadida nueva tipografía para mostrar los puntos acumulados

--- core/src/com/revoltagames/projectsquare/GameStates/Menu.java
+++ core/src/com/revoltagames/projectsquare/GameStates/Menu.java
@@ -2,6 +2,8 @@ package com.revoltagames.projectsquare.GameStates;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.revoltagames.projectsquare.Entities.Button;
 import com.revoltagames.projectsquare.Entities.Shapes.Circle;
@@ -16,10 +18,11 @@ import com.revoltagames.projectsquare.ProjectSquare;
 public class Menu extends GameState {
 
     private ShapeRenderer shapeR;
+    private SpriteBatch renderer;
     private Button playB;
     private Button highScores;
     private Music track;
-
+    private BitmapFont font;
 
     public Menu(GameStateManager gsm) {
         super(gsm);
@@ -39,6 +42,8 @@ public class Menu extends GameState {
         ));
         playB = new Button();
         highScores = new Button(ProjectSquare.WIDTH/2, ProjectSquare.HEIGTH/2);
+        font = ProjectSquare.resManager.getFont(3);
+        renderer = new SpriteBatch();
     }
 
     @Override
@@ -50,6 +55,14 @@ public class Menu extends GameState {
     public void draw() {
         playB.draw(shapeR);
         highScores.draw(shapeR);
+
+        int score = ProjectSquare.dataManager.getCoins();
+        String puntuacion = "puntos: " + score;
+        BitmapFont.TextBounds bounds = font.getBounds(puntuacion);
+
+        renderer.begin();
+        font.draw(renderer,puntuacion, ProjectSquare.WIDTH/2 - bounds.width/2, ProjectSquare.HEIGTH - ProjectSquare.HEIGTH/12 - bounds.height/2);
+        renderer.end();
     }
 
     @Override
